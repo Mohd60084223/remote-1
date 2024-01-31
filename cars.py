a0ed9fa93e77159dc506b0fae30f22022f9816a8
Mohamed,Mohamed <60084223@udst.edu.qa>
2024-01-31 10:40:02 +0300

    Update 1

--- cars.py
+++ cars.py
@@ -1,5 +1,9 @@
-def greeting():
-	print("Hello")
+import random
+def select_random():
+	car_brands = ["Toyota","Nissan","Ford"]
+	random_brand = random.choice(car_brands)
+	return random_brand
 
-if __name__ == "__main__":
-	greeting()
+if __name__ = "__main"":
+	random_car_brand = select_random()
+	print("Random cars is: ", random_car_brans)
