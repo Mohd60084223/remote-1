b7833d40689b8fe7dc7ceb356e7f8fd35c53d416
Mohamed,Mohamed <60084223@udst.edu.qa>
2024-01-31 10:58:30 +0300

    Car year

--- cars.py
+++ cars.py
@@ -1,9 +1,13 @@
 import random
 def select_random():
 	car_brands = ["Toyota","Nissan","Ford"]
+	model_year = [1996,2020,2024]
 	random_brand = random.choice(car_brands)
+	random_year = random.choice(model_year)
+	return random_year
 	return random_brand
 
 if __name__ = "__main"":
 	random_car_brand = select_random()
-	print("Random cars is: ", random_car_brans)
+	random_car_year = select_random()
+	print("Random car and date are: ", random_car_brans, random_car_year)
